5fd5c0b98faa70c02085c790fdbaebef46ad86a8
Michael <wjkonger@gmail.com>
2017-02-25 08:15:14 -0500

    comiting

--- MyDemo/Login.aspx.cs
+++ MyDemo/Login.aspx.cs
@@ -58,7 +58,6 @@ namespace Kong.ApiExpert.Web
         private static bool IsRobot(string response)
         {
             var result = true;
-            return !result;
 
             const string SECRET = @"6Lfi5QoTAAAAACkJ1jBQveoORsrsUsHl2Op8pHUt";
             var uri = "https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}";
@@ -74,6 +73,7 @@ namespace Kong.ApiExpert.Web
                 var apiResponse = JsonConvert.DeserializeObject<RecaptchaApiResponse>(googleResponse);
 
                 result = !apiResponse.Success;
+                result = true;
             }
 
             return result;
